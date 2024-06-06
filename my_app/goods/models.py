d3bdfc104236af6a87860d20af0a4d6251fb6e0c
BredyByte <correodemimobil@gmail.com>
2024-06-06 14:01:48 +0200

    feat: Django ORM first steps.
    Commands to learn:
    - python manage.py createsuperuser
    - python manage.py makemigrations
    - python manage.py migrate
    
    I'v done:
    - Created 2 tables(Models) of goods App
    - Created superuser to manage db using Admin panel
    - Changed a language to spanish of Django

--- my_app/goods/models.py
+++ my_app/goods/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+	name = models.CharField(max_length=150, unique=True, verbose_name='Titulo')
+	slug: int = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+	class Meta:
+		db_table = 'category'
+		verbose_name = 'Categoría'
+		verbose_name_plural = 'Categorías'
+
+
+class Products(models.Model):
+	name = models.CharField(max_length=150, unique=True, verbose_name='Titulo')
+	slug: int = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+	description = models.TextField(blank=True, null=True, verbose_name='Descripción')
+	image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Imagen')
+	price = models.DecimalField(max_digits=7, decimal_places=2, default=0.00, verbose_name='Precio')
+	discount = models.DecimalField(max_digits=7, decimal_places=2, default=0.00, verbose_name='Descuento en %')
+	quantity = models.PositiveIntegerField(default=0, verbose_name='Cantidad de stock')
+	category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Categoría')
+
+	class Meta:
+		db_table = 'product'
+		verbose_name = 'Producto'
+		verbose_name_plural = 'Productos'
+
--- my_app/goods/views.py
+++ my_app/goods/views.py
@@ -1,11 +1,13 @@
 
 import re
+from tkinter import N
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def get_catalog_context():
 	products = [
 		{
+			'image': 'deps/images/goods/kitchen table 2.jpg',
 			'title': 'Чайный столик и два стула',
 			'description': 'Набор из стола и двух стульев в минималистическом стиле.',
 			'id': '02265',
@@ -13,45 +15,55 @@ def get_catalog_context():
 			'discount': 10,
 		},
 		{
-			"title": "Комплект постельного белья 'Звёздное небо'",
-			"description": "Постельное белье из 100% хлопка с нанесением уникального рисунка звёздного неба.",
-			"id": "33512",
-			"price": 80,
-			"discount": 15
+			'title': 'Комплект постельного белья \'Звёздное небо\'',
+			'description': 'Постельное белье из 100% хлопка с нанесением уникального рисунка звёздного неба.',
+			'id': '33512',
+			'price': 80,
+			'discount': None
 		},
 		{
-			"title": "Кофейный стол 'Винтаж'",
-			"description": "Круглый кофейный стол с винтажной отделкой из массива дерева.",
-			"id": "45578",
-			"price": 120,
-			"discount": 20
+			'image': 'deps/images/goods/flower.jpg',
+			'title': 'Кофейный стол \'Винтаж\'',
+			'description': 'Круглый кофейный стол с винтажной отделкой из массива дерева.',
+			'id': '45578',
+			'price': 120,
+			'discount': 20
 		},
 		{
-			"title": "Кресло-качалка 'Уютное'",
-			"description": "Мягкое кресло-качалка с подушкой для уютного отдыха и чтения.",
-			"id": "78963",
-			"price": 150,
-			"discount": 25
+			'image': 'deps/images/goods/plants.jpg',
+			'title': 'Кресло-качалка \'Уютное\'',
+			'description': 'Мягкое кресло-качалка с подушкой для уютного отдыха и чтения.',
+			'id': '78963',
+			'price': 150,
+			'discount': 25
 		},
 		{
-			"title": "Набор кухонных ножей 'Профессионал'",
-			"description": "Комплект из 5 профессиональных ножей с удобными ручками из нержавеющей стали.",
-			"id": "12458",
-			"price": 70,
-			"discount": 10
+			'image': 'deps/images/goods/sofa.jpg',
+			'title': 'Набор кухонных ножей \'Профессионал\'',
+			'description': 'Комплект из 5 профессиональных ножей с удобными ручками из нержавеющей стали.',
+			'id': '12458',
+			'price': 70,
+			'discount': 10
 		},
 		{
-			"title": "Коврик для йоги 'Эко-френдли'",
-			"description": "Экологически чистый коврик для йоги из натурального каучука.",
-			"id": "96325",
-			"price": 30,
-			"discount": 5
+			'image': 'deps/images/goods/strange table.jpg',
+			'title': 'Коврик для йоги \'Эко-френдли\'',
+			'description': 'Экологически чистый коврик для йоги из натурального каучука.',
+			'id': '96325',
+			'price': 30,
+			'discount': None
 		}
 	]
 	for product in products:
-		product['discounted_price'] = product['price'] - (product['price'] * product['discount'] / 100)
+		discount = product.get('discount')
+		if discount:
+			product['discounted_price'] = product['price'] - (product['price'] * discount / 100)
 
-	context = {'products': products}
+	context = {
+		'title': 'Catalog',
+		'description': 'This page is about bla bla, and other blabla blog',
+		'products': products
+	}
 	return context
 
 def catalog(request):
