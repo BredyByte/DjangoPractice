b568ae960d595d025158100a8a1034306319916d
BredyByte <correodemimobil@gmail.com>
2024-06-10 14:07:39 +0200

    feat: Media for db is configured

--- my_app/app/urls.py
+++ my_app/app/urls.py
@@ -16,17 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
-from app.settings import DEBUG
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
-    path('admin/', admin.site.urls),
-	path("__debug__/", include("debug_toolbar.urls")),
+    path('admin/', admin.site.urls)
 ]
 
-if DEBUG:
+if settings.DEBUG:
 	urlpatterns += [
 		path("__debug__/", include("debug_toolbar.urls")),
 	]
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- my_app/goods/views.py
+++ my_app/goods/views.py
@@ -9,69 +9,24 @@ from . import views
 from goods.models import Categories, Products
 
 def catalog(request):
-	def get_catalog_context():
-		products = [
-			{
-				'image': 'deps/images/goods/kitchen table 2.jpg',
-				'title': 'Чайный столик и два стула',
-				'description': 'Набор из стола и двух стульев в минималистическом стиле.',
-				'id': '02265',
-				'price': 100,
-				'discount': 10,
-			},
-			{
-				'title': 'Комплект постельного белья \'Звёздное небо\'',
-				'description': 'Постельное белье из 100% хлопка с нанесением уникального рисунка звёздного неба.',
-				'id': '33512',
-				'price': 80,
-				'discount': None
-			},
-			{
-				'image': 'deps/images/goods/flower.jpg',
-				'title': 'Кофейный стол \'Винтаж\'',
-				'description': 'Круглый кофейный стол с винтажной отделкой из массива дерева.',
-				'id': '45578',
-				'price': 120,
-				'discount': 20
-			},
-			{
-				'image': 'deps/images/goods/plants.jpg',
-				'title': 'Кресло-качалка \'Уютное\'',
-				'description': 'Мягкое кресло-качалка с подушкой для уютного отдыха и чтения.',
-				'id': '78963',
-				'price': 150,
-				'discount': 25
-			},
-			{
-				'image': 'deps/images/goods/sofa.jpg',
-				'title': 'Набор кухонных ножей \'Профессионал\'',
-				'description': 'Комплект из 5 профессиональных ножей с удобными ручками из нержавеющей стали.',
-				'id': '12458',
-				'price': 70,
-				'discount': 10
-			},
-			{
-				'image': 'deps/images/goods/strange table.jpg',
-				'title': 'Коврик для йоги \'Эко-френдли\'',
-				'description': 'Экологически чистый коврик для йоги из натурального каучука.',
-				'id': '96325',
-				'price': 30,
-				'discount': None
-			}
-		]
-		for product in products:
-			discount = product.get('discount')
-			if discount:
-				product['discounted_price'] = product['price'] - (product['price'] * discount / 100)
+	products = Products.objects.all()
 
-		context = {
-			'title': 'Catalog',
-			'description': 'This page is about bla bla, and other blabla blog',
-			'products': products
-		}
-		return context
+	modified_products = []
 
-	return render(request, 'goods/catalog.html', get_catalog_context())
+	for product in products:
+		discount = product.discount
+		if discount:
+			discounted_price = product.price - (product.price * discount / 100)
+			product.discount_price = discounted_price
+		modified_products.append(product)
+
+	context = {
+		'title': 'Catalog',
+		'description': 'This page is about bla bla, and other blabla blog',
+		'products': modified_products
+	}
+
+	return render(request, 'goods/catalog.html', context)
 
 class MyTestView(generic.ListView):
 	template_name = "goods/testPage.html"
