0b7e783974b60873b0879a0d9c251f4ca4511fd3
BredyByte <correodemimobil@gmail.com>
2024-06-10 10:25:28 +0200

    feat: Debug_toolbar is added

--- my_app/app/settings.py
+++ my_app/app/settings.py
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'main',
-	'goods',
+
+    "debug_toolbar",
 	'channels',
+
+    'main',
+	'goods',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -125,6 +131,9 @@ STATICFILES_DIRS =  [
 	BASE_DIR / 'static'
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- my_app/app/urls.py
+++ my_app/app/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from app.settings import DEBUG
+
 urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+	path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if DEBUG:
+	urlpatterns += [
+		path("__debug__/", include("debug_toolbar.urls")),
+	]
