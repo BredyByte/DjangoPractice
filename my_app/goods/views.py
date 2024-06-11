6c790cbc7bbe024311599df62da53e2ba0c81a53
BredyByte <correodemimobil@gmail.com>
2024-06-11 15:28:24 +0200

    feat: slug converter to categories

--- my_app/goods/views.py
+++ my_app/goods/views.py
@@ -2,14 +2,17 @@
 import re
 from tkinter import N
 from django.http import Http404, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 from django.views import generic
 from . import views
 
 from goods.models import Categories, Products
 
-def catalog(request):
-	products = Products.objects.all()
+def catalog(request, category_slug):
+	if category_slug == 'all':
+		products = Products.objects.all()
+	else:
+		products = get_list_or_404( Products.objects.filter(category__slug=category_slug))
 
 	context = {
 		'title': 'Catalog',
