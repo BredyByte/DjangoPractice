6fddbf8fd5d587253d937be444cdda4e36989da0
BredyByte <correodemimobil@gmail.com>
2024-06-11 20:14:11 +0200

    feat: Product filter with pagination and urlencode

--- my_app/goods/views.py
+++ my_app/goods/views.py
@@ -10,11 +10,18 @@ from goods.models import Categories, Products
 def catalog(request, category_slug):
 
 	page = request.GET.get('page', 1)
+	on_sale = request.GET.get('on_sale', None)
+	order_by = request.GET.get('order_by', None)
 
 	if category_slug == 'all':
 		products = Products.objects.all()
 	else:
 		products = get_list_or_404( Products.objects.filter(category__slug=category_slug))
+
+	if on_sale:
+		products = products.filter(discount__gt=0)
+	if order_by and order_by != 'default':
+		products = products.order_by(order_by)
 
 	paginator = Paginator(products, 3)
 	current_page = paginator.page(int(page))
@@ -25,8 +32,6 @@ def catalog(request, category_slug):
 		'products': current_page,
 		'slug_url': category_slug,
 	}
-
-
 
 	return render(request, 'goods/catalog.html', context)
 
