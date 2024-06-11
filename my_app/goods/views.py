6485349b79c371b05751f9adfde67051e3eb02af
BredyByte <correodemimobil@gmail.com>
2024-06-11 18:18:29 +0200

    feat: second  pagination option with get paramets that are handled in views.py
    You can see all paramets of request parametr of every view.py function
    using debug VSC mode and hover over it by mouse.

--- my_app/goods/views.py
+++ my_app/goods/views.py
@@ -5,25 +5,28 @@ from django.core.paginator import Paginator
 from django.http import Http404, HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 from django.views import generic
-from . import views
-
 from goods.models import Categories, Products
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+	page = request.GET.get('page', 1)
+
 	if category_slug == 'all':
 		products = Products.objects.all()
 	else:
 		products = get_list_or_404( Products.objects.filter(category__slug=category_slug))
 
 	paginator = Paginator(products, 3)
-	current_page = paginator.page(page)
+	current_page = paginator.page(int(page))
 
 	context = {
 		'title': 'Catalog',
 		'description': 'This page is about bla bla, and other blabla blog',
 		'products': current_page,
-		'slug_url': category_slug
+		'slug_url': category_slug,
 	}
+
+
 
 	return render(request, 'goods/catalog.html', context)
 
