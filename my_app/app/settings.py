8e2496caa6b629f7ce996c3028b3cda0e4d21df8
BredyByte <correodemimobil@gmail.com>
2024-06-12 18:08:27 +0200

    feat: Search is added is changed. PostgresSQL.
    Search with django django.contrib.postgres.search library.

--- my_app/app/settings.py
+++ my_app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     "debug_toolbar",
 	'channels',
@@ -87,8 +88,12 @@ ASGI_APPLICATION = 'app.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testproject',
+		'USER': 'dbredykh',
+		'PASSWORD': 'dbredykhpass',
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
     }
 }
 
@@ -115,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- my_app/goods/utils.py
+++ my_app/goods/utils.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 from django.db.models import Q
 from goods.models import Products
 
@@ -6,12 +7,31 @@ def q_search(query):
 	if query.isdigit() and len(query) <= 5:
 		return Products.objects.filter(id=int(query))
 
-	keywords = [word for word in query.split() if len(word) > 2]
+	vector = SearchVector("name", "description")
+	query = SearchQuery(query)
 
-	q_objects = Q()
+	res = (
+		Products.objects.annotate(rank=SearchRank(vector, query))
+		.filter(rank__gt=0)
+		.order_by("-rank")
+	)
 
-	for token in keywords:
-		q_objects |= Q(description__icontains=token)
-		q_objects |= Q(name__icontains=token)
+	res = res.annotate(
+		headline=SearchHeadline(
+			"name",
+			query,
+			start_sel='<span style="background-color: yellow;">',
+			stop_sel='</span>'
+		)
+	)
 
-	return Products.objects.filter(q_objects)
+	res = res.annotate(
+		bodyline=SearchHeadline(
+			"description",
+			query,
+			start_sel='<span style="background-color: yellow;">',
+			stop_sel='</span>'
+		)
+	)
+
+	return res
