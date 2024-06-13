49dd2c58af8616015e48c8dbe8a679c6d079b807
BredyByte <correodemimobil@gmail.com>
2024-06-13 20:15:50 +0200

    feat: Authorization is done. Advanced technics
    Using inheritance of the AuthenticationForm, you can change the behavior
    of the form and its fields. There are various ways to implement it. Some
    commented examples are included.

--- my_app/users/forms.py
+++ my_app/users/forms.py
@@ -1,9 +1,33 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
 	class Meta:
 		model = User()
+		fields = ['username', 'password']
+
+	"""
+	Переназначение полей. В первом случае просто оставляем пустыми, во втором добавляем всю необоходимую доп инфу:
+	плейсхолдеры и тд. Чтобы детально посмотреть как это деалется нажди на cntrl и левой кнобкой по AuthenticationForm
+	"""
+	# username = forms.CharField()
+	# password = forms.CharField()
+
+	# username = forms.CharField(
+	# 	label = 'Name',
+	# 	widget = forms.TextInput(attrs = {
+	# 		'autofocus': True,
+	# 		'class': 'form-control',
+	# 		'placeholder': 'Enter here your username plz!'
+	# 	})
+	# )
+	# pusername = forms.CharField(
+	# 	label = 'Name',
+	# 	widget = forms.PasswordInput(attrs = {
+	# 		'autocomplete': 'current-password',
+	# 		'class': 'form-control',
+	# 		'placeholder': 'Enter here your password plz!'
+	# 	})
+	# )
