8ccb49316664721a7d32a8d9214c619373a78a7d
BredyByte <correodemimobil@gmail.com>
2024-06-14 14:02:55 +0200

    feat: Registration form and controller is ready
    Class for registration form is inherited from django.contrib.auth.forms
    UserCreationForm. This is an intermediate commit, as the user's profile
    page has not been created yet. In this case, you can check trough the db
    that the user is created.

--- my_app/users/views.py
+++ my_app/users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
 	if request.method == 'POST':
@@ -27,8 +27,24 @@ def login(request):
 	return render(request, 'users/login.html', context)
 
 def registration(request):
+
+	if request.method == 'POST':
+		form = UserRegistrationForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			"""
+			Для того чтобы автологинить юзера, если не хочешь автологинить,
+			убери эту линию и редиректи на авторизационную страницу
+			"""
+			user = form.instance
+			auth.login(request, user)
+			return HttpResponseRedirect(reverse('main:index'))
+	else:
+		form = UserRegistrationForm()
+
 	context = {
 		'title': 'Home - Registration',
+		'form': form
 	}
 
 	return render(request, 'users/registration.html', context)
@@ -41,4 +57,5 @@ def profile(request):
 	return render(request, 'users/profile.html', context)
 
 def logout(request):
-	pass
+	auth.logout(request)
+	return redirect(reverse('main:index'))
