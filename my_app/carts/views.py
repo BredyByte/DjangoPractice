f17ab4ceb1ca5cf5ec305ba69898b10def736445
BredyByte <correodemimobil@gmail.com>
2024-06-17 10:41:59 +0200

    feat: Add-to-cart ajax request and json response

--- my_app/carts/views.py
+++ my_app/carts/views.py
@@ -1,9 +1,19 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from goods.models import Products
 from carts.models import Cart
+from carts.utils import get_user_carts
 
-def cart_add(request, product_slug):
-	product = Products.objects.get(slug=product_slug)
+"""
+	HTTP_REFERER -> ключ отвечающий за страницу с которой мы попали на эту.
+	Данное выражение нас перенаправляет на предыдущую страницу
+	return redirect(request.META['HTTP_REFERER'])
+"""
+
+def cart_add(request):
+	product_id = request.POST.get("product_id")
+	product = Products.objects.get(id=product_id)
 
 	if request.user.is_authenticated:
 		carts = Cart.objects.filter(user=request.user, product=product)
@@ -16,11 +26,17 @@ def cart_add(request, product_slug):
 	else:
 		Cart.objects.create(user=request.user, product=product, quantity=1)
 
-	"""
-	HTTP_REFERER -> ключ отвечающий за страницу с которой мы попали на эту.
-	Данное выражение нас перенаправляет на предыдущую страницу
-	"""
-	return redirect(request.META['HTTP_REFERER'])
+	user_cart = get_user_carts(request)
+	cart_items_html = render_to_string(
+		"carts/includes/included_cart.html", {"carts": user_cart}, request=request
+	)
+
+	response_data = {
+		"message": "El producto esta añadido",
+		"cart_items_html": cart_items_html
+	}
+
+	return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
 	pass
